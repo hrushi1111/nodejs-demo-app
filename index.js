43486e1d6de8317c077401c9c4233c8cf6816e68
hrushi1111 <hrushikeshguntupalli1@gmail.com>
2025-09-23 15:06:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello from nodejs-demo-app!'));
+app.get('/', (req, res) => res.send('deployment of applicaton is done successfully!'));
+app.get('/', (req, res) => res.send('using github actions'));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
